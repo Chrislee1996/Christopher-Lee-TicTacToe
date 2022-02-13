28ade5737ceb30328511047a24f05cc6df412e98
chrislee1996 <prepchrislee@gmail.com>
2022-02-12 22:29:21 -0500

    updated CSS and added click to display x or o depending on players turn

--- js/script.js
+++ js/script.js
@@ -1,100 +1,75 @@
-// A user should be able to click on different squares to make a move.
-    //need to add event listener for click on our grid 
-// A cell should not be able to be replayed once marked.
-    // use if statement to log error if grid is already marked 
-// You should not be able to click remaining empty cells after the game is over.
-    // use if statement to log error if no more grids are availab.e 
-// Add a reset button that will clear the contents of the board.
-    //need to add event listener for click to clean board 
-// Display a message to indicate which turn is about to be played.
-// Detect draw conditions (ties/cat's game)
-    //need to create an if statement that causes a tie if no more grids are availabe
-// Detect winner: Stop game and declare the winner if one player ends up getting three in a row.
-    //create coniditon where if 3 of an x or o is met, game is over 
-// Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
-//need to create a click handler that will add either a circle or x once clicked 
-    //use event
-// Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
-    //need to create div for game choices 
-    // need to add game choices to css to show on webpage
-// Every click will alternate between marking an X and O
-    // need to make if statement to make sure if one is done, then go to next
-    //also need to make a for loop so once it is compeleted, we cycle through the options until condition is met
-//add eventlistener to document
+const playerOne = 'O'
+const playerTwo = 'X'
+let firstTurn = playerOne
+const gameBoard = [["", "", "",
+"", "", "",
+"", "", ""
+]]
 
-
-const grid = document.querySelector('#grid')
+const winningBoard= [[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6], 
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]]
+const board = document.querySelector('#container')
 const turnMessage = document.querySelector('#turnMessage')
 const resetTheGame = document.querySelector('#resetTheGame')
-const results = document.querySelector('#results')
+const result = document.querySelector('#result')
+let gameOver = false
 
-//create an array for the grid rows 
-const gridRows = ['','','',
-'','','',
-'','','']
+//create event listener that will display "something" when clicked
+const gridClicked = (event) => {  
+    //add something to our grid via click
+    const grid = event.target
+    console.log("this shows what grid is being clicked",grid)
+    //create condition on turns and result of click
+        //if clicked, need to display 'something'
+        //switch to player2 after click
+        //repeat and then switchn back to player1
+     if (firstTurn === playerOne) {
+        grid.innerText=playerOne
+       gameBoard == playerOne
+       firstTurn = playerTwo
+       turnMessage.innerText= `Player 1 has played their O! It is now player's 2 turn`
+    } else { 
+        grid.innerText = playerTwo
+       gameBoard == playerTwo
+       firstTurn = playerOne
+       turnMessage.innerText= `Player 2 has played X! It is now player's 1 turn`
+    } 
+    let gameWon = checkWin()
 
-//create a function that will display choices when clicked
-const choiceList = () => {
-    console.log('this shows me that a grid has been clicked via the console log')
-//remove any shapes already in grid before start
-while (grid.firstChild) {
-    grid.removeChild(grid.firstChild)
-}
-const circle = document.createElement('div')
-circle.classList.add('circle')
-grid.appendChild(circle)
-const notCircle = document.createElement('div')
-notCircle.classList.add('notCircle')
-grid.appendChild(notCircle)
-}
-
-//if there is already a shape in a grid, no more can enter
-const gridLimit = () => {
-    while (grid.firstChild) {
-        grid.removeChild(grid.firstChild)
-    }
 }
 
-//create eventlistener to document
-//create event listener when a grid is clicked, to show player action
-document.addEventListener('DOMContentLoaded', () => {
-    grid.addEventListener('click', choiceList)
-    choiceList()
-    gridLimit()
+//create func to check for wins 
+const checkWin = () =>{
+for (let i =0; i <winningBoard.length;i++) {
+    if (winningBoard[i][0] == winningBoard[i][1] && winningBoard[i][0] == winningBoard[i][2] && winningBoard[i][2] ==winningBoard[i][3] && winningBoard[i][0] !== '')
+    document.getElementById('result').innerText='winner'
+}
+}
+    
+
+    document.addEventListener('DOMContentLoaded', () => {
+        //create event listener that for grid clicks 
+    const gridElements = document.querySelectorAll('.grid').forEach(grid => grid.addEventListener('click', gridClicked, {once:true}))  
 })
 
 
 
-
-
-//create a function that will display o when clicked
-// const choiceCircle = () => {
-//     console.log('this shows me that a grid has been clicked via the console log')
-//         //if there are any shapes in grid before load - remove them
-//     while (grid.firstChild) {
-//         grid.removeChild(grid.firstChild)
-//     }
-//     //generate a div every click 
-//     const circle = document.createElement('div')
-//     //loop the grid rows so we can click on every grid 
-//     //make that div a circle
-//     circle.classList.add('circle')
-//     //append the circle into the grid
-//     grid.appendChild(circle)
-// }
-
-// //create a function that will display another shape (can't create an x!) when clicked
-// const choiceSquare = () => {
-//     console.log('this shows me that a grid has been clicked via the console log')
-//     //if there are any shapes in grid before load - remove them
-//     while (grid.firstChild) {
-//         grid.removeChild(grid.firstChild)
-//     }
-//     //generate a div every click 
-//     const notCircle = document.createElement('div')
-//     //loop the grid rows so we can click on every grid 
-//     //make that div a circle
-//     notCircle.classList.add('notCircle')
-//     //append the circle into the grid
-//     grid.appendChild(notCircle)
-// }
+//need to create player variables
+//create var for choices 
+//create variable on how to get winner
+// A user should be able to click on different squares to make a move.
+// Every click will alternate between marking an X and O
+// Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
+// A cell should not be able to be replayed once marked.
+// You should not be able to click remaining empty cells after the game is over.
+// Add a reset button that will clear the contents of the board.
+// Display a message to indicate which turn is about to be played.
+// Detect draw conditions (ties/cat's game)
+// Detect winner: Stop game and declare the winner if one player ends up getting three in a row.
+// Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
